c7147d6668213f24b0c88e763908dd19463ceb81
khaleghi78 <khaleghirt@gmail.com>
2023-01-02 17:41:00 +0330

    third

--- src/components/home/details/Details.jsx
+++ src/components/home/details/Details.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useHistory, useParams } from 'react-router-dom'
-import { MdDeleteSweep, MdStarRate } from 'react-icons/md'
+import { MdStarRate } from 'react-icons/md'
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai'
 import { ADD, DELETE, REMOVE_INT } from '../../../controller/action'
 
